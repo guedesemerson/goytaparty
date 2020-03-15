49fe6c080e332dd5d16bb141549ad5507611e2cf
guedesemerson <guedes.emerson@hotmail.com>
2020-03-15 19:56:12 -0300

    Configuring the app

--- guestList/urls.py
+++ guestList/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('convidados/', include('convidado.urls')),
     path('eventos/', include('evento.urls')),
-    path('', include('django.contrib.auth.urls')),
 ]
