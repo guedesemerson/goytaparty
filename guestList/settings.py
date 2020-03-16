28cbf72904b4f6b1e235718248248f857e21e117
guedesemerson <guedes.emerson@hotmail.com>
2020-03-15 21:05:13 -0300

    Configuring the app

--- guestList/settings.py
+++ guestList/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1','goytaparties.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','goytaparties.herokuapp.com','0.0.0.0']
 
 
 # Application definition
