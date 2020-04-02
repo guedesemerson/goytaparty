a28f320bbb2a1726968883ca1d11a4c45d0d1c13
guedesemerson <guedes.emerson@hotmail.com>
2020-04-02 16:32:11 -0300

    incrementando documentação

--- evento/tests.py
+++ evento/tests.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from evento.models import Evento
 
-# Create your tests here.
+class EventoTestCase(TestCase):
+    def setUp(self):
+        userEmerson = User.objects.get(id=2)
+        Evento.objects.create(user=userEmerson, nome_festa="festaTeste", data="24/03/2020", local="Rio das Ostras", endereco="Jardim Bela vista")
+
+
+    def test_ValicaEvento(self):
+        assert Evento.objects.all().count() == 1
